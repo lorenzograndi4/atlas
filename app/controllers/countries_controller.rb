3aa59418f1a9c6f15dd73e8577ede2d50330dbd9
BJelenjev <boacon1@yahoo.com>
2017-07-01 14:17:38 +0200

    added edit and create country

--- app/controllers/countries_controller.rb
+++ app/controllers/countries_controller.rb
@@ -5,4 +5,30 @@ class CountriesController < ApplicationController
  def show
    @country = Country.find(params[:id])
  end
+ def new
+   @country = Country.new
+ end
+ def update
+    @country = Country.find(params[:id])
+
+    country_params = params.require(:country).permit(:name, :population, :flag_url, :continent_id)
+
+    if @country.update_attributes(country_params)
+      redirect_to @country
+    else
+      render 'edit'
+    end
+  end
+  def edit
+    @country = Country.find(params[:id])
+  end
+  def create
+    country_params = params.require(:country).permit(:name, :population, :flag_url, :continent_id)
+    @country = Country.new(country_params)
+    if @country.save
+      redirect_to @country
+    else
+      render 'new'
+    end
+  end
 end
