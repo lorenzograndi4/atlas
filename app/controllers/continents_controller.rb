17a200c329debed35392238de3308673f9f2d1b3
maria.marica <maria.marica03@gmail.com>
2017-07-01 14:12:17 +0200

    added edit delete methods + form

--- app/controllers/continents_controller.rb
+++ app/controllers/continents_controller.rb
@@ -20,4 +20,25 @@ class ContinentsController < ApplicationController
       render 'edit'
     end
   end
+
+  def update
+    @continent = Continent.find(params[:id])
+
+    if @continent.update_attributes(continent_params)
+       redirect_to @continent
+    else
+       render 'edit'
+    end
+  end
+
+  def delete
+    @continent = Continent.find(params[:id])
+    @continent.destroy
+    redirect_to  continent_path
+  end
+
+  private
+
+  @continent_params = params.require(:continent).permit(:name, :image_url)
+  
 end
